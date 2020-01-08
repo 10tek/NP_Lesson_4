43752744cbc8bf652f85e28d354daa3e80640f9b
10tekinvo@gmail.com <10tekinvo@gmail.com>
2020-01-08 20:00:37 +0600

    Lesson commit

--- UdpLesson/UdpLesson/Program.cs
+++ UdpLesson/UdpLesson/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 
 namespace UdpLesson
 {
@@ -6,7 +9,22 @@ namespace UdpLesson
     {
         static void Main(string[] args)
         {
+            var udpClient = new UdpClient(3231);
+            IPEndPoint sender = null;
+            var result = udpClient.Receive(ref sender);
 
+            // для мультикаст сообщений
+            // udpClient.JoinMulticastGroup(IPAddress.Parse("127.0.0.1"));
+            udpClient.Close();
+
+            var senderUdp = new UdpClient(3231);
+            var text = "Hello";
+            var bytes = System.Text.Encoding.UTF8.GetBytes(text);
+            var sendedBytes = senderUdp.Send(bytes, bytes.Length);
+            senderUdp.Close();
+
+            Console.ReadKey();
         }
+
     }
 }
